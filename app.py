218fc9676d86074e44b9a517fa8ac5669bd59d8c
Narek201114 <galstyannarek704@gmail.com>
2026-06-14 14:16:11 +0400

    Reorganize routes and add weather page

--- app.py
+++ app.py
@@ -3,20 +3,24 @@ from pymongo import MongoClient
 import os
 
 app = Flask(__name__)
+# Օգտագործում ենք Render-ի Environment-ում սահմանված գաղտնի բանալին
 app.secret_key = os.environ.get('FLASK_SECRET_KEY')
 
-# Միացում MongoDB-ին՝ օգտագործելով Render-ի Environment փոփոխականը
+# Միացում MongoDB-ին՝ օգտագործելով MONGO_URI-ն
 client = MongoClient(os.environ.get('MONGO_URI'))
 db = client.weather_db
-
-@app.route('/login')
-def login():
-    # Սա կբացի templates/login.html ֆայլը
-    return render_template('login.html')
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/weather')
+def weather():
+    return render_template('weather.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
